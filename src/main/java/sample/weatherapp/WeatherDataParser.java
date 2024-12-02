2e64d591f6239c04444588b05791c71b7324f19e
olgaDCI <olga.babina@dci-student.org>
2024-12-02 11:02:05 +0100

    Fixed bug with wrong Json response

--- src/main/java/sample/weatherapp/WeatherDataParser.java
+++ src/main/java/sample/weatherapp/WeatherDataParser.java
@@ -68,38 +68,37 @@ public class WeatherDataParser {
 
   private static Forecast getForecast(JsonNode node) {
 
-    long dt = node.get("dt").asLong();
-    JsonNode main = node.get("main");
-    double temp = main.get("temp").asDouble() - 273.15;
-    double feelsLike = main.get("feels_like").asDouble() - 273.15;
-    double tempMin = main.get("temp_min").asDouble() - 273.15;
-    double tempMax = main.get("temp_max").asDouble() - 273.15;
-    int pressure = main.get("pressure").asInt();
-    int seaLevel = main.get("sea_level").asInt();
-    int grndLevel = main.get("grnd_level").asInt();
-    int humidity = main.get("humidity").asInt();
-    double tempKf = main.get("temp_kf").asDouble();
+      long dt = node.has("dt") ? node.get("dt").asLong() : 0L;
+      JsonNode main = node.has("main") ? node.get("main") : null;
+      double temp = main != null && main.has("temp") ? main.get("temp").asDouble() - 273.15 : 0.0;
+      double feelsLike = main != null && main.has("feels_like") ? main.get("feels_like").asDouble() - 273.15 : 0.0;
+      double tempMin = main != null && main.has("temp_min") ? main.get("temp_min").asDouble() - 273.15 : 0.0;
+      double tempMax = main != null && main.has("temp_max") ? main.get("temp_max").asDouble() - 273.15 : 0.0;
+      int pressure = main != null && main.has("pressure") ? main.get("pressure").asInt() : 0;
+      int seaLevel = main != null && main.has("sea_level") ? main.get("sea_level").asInt() : 0;
+      int grndLevel = main != null && main.has("grnd_level") ? main.get("grnd_level").asInt() : 0;
+      int humidity = main != null && main.has("humidity") ? main.get("humidity").asInt() : 0;
+      double tempKf = main != null && main.has("temp_kf") ? main.get("temp_kf").asDouble() : 0.0;
 
-    JsonNode weather = node.get("weather").get(0);
-    String weatherMain = weather.get("main").asText();
-    String weatherDescription = weather.get("description").asText();
-    String weatherIcon = weather.get("icon").asText();
+      JsonNode weather = node.has("weather") && node.get("weather").size() > 0 ? node.get("weather").get(0) : null;
+      String weatherMain = weather != null && weather.has("main") ? weather.get("main").asText() : "";
+      String weatherDescription = weather != null && weather.has("description") ? weather.get("description").asText() : "";
+      String weatherIcon = weather != null && weather.has("icon") ? weather.get("icon").asText() : "";
 
-    int cloudsAll = node.get("clouds").get("all").asInt();
-    double windSpeed = node.get("wind").get("speed").asDouble();
-    int windDeg = node.get("wind").get("deg").asInt();
-    double windGust = node.get("wind").get("gust").asDouble();
-    int visibility = node.get("visibility").asInt();
-    double pop = node.get("pop").asDouble();
-    String pod = node.get("sys").get("pod").asText();
-    String dtTxt = node.get("dt_txt").asText();
+      int cloudsAll = node.has("clouds") && node.get("clouds").has("all") ? node.get("clouds").get("all").asInt() : 0;
+      double windSpeed = node.has("wind") && node.get("wind").has("speed") ? node.get("wind").get("speed").asDouble() : 0.0;
+      int windDeg = node.has("wind") && node.get("wind").has("deg") ? node.get("wind").get("deg").asInt() : 0;
+      double windGust = node.has("wind") && node.get("wind").has("gust") ? node.get("wind").get("gust").asDouble() : 0.0;
+      int visibility = node.has("visibility") ? node.get("visibility").asInt() : 0;
+      double pop = node.has("pop") ? node.get("pop").asDouble() : 0.0;
+      String pod = node.has("sys") && node.get("sys").has("pod") ? node.get("sys").get("pod").asText() : "";
+      String dtTxt = node.has("dt_txt") ? node.get("dt_txt").asText() : "";
 
-    Forecast forecast = new Forecast(
-        dt, temp, feelsLike, tempMin, tempMax, pressure, seaLevel, grndLevel, humidity, tempKf,
-        weatherMain, weatherDescription, weatherIcon, cloudsAll, windSpeed, windDeg, windGust,
-        visibility, pop, pod, dtTxt
-    );
-    return forecast;
+      return new Forecast(
+          dt, temp, feelsLike, tempMin, tempMax, pressure, seaLevel, grndLevel, humidity, tempKf,
+          weatherMain, weatherDescription, weatherIcon, cloudsAll, windSpeed, windDeg, windGust,
+          visibility, pop, pod, dtTxt
+      );
   }
 
 
