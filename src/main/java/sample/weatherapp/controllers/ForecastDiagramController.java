0d9c2794f08f4494f7c4a6aa29ad577f740068f1
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-12-03 13:28:40 +0100

    call diagram logic outside UI thread

--- src/main/java/sample/weatherapp/controllers/ForecastDiagramController.java
+++ src/main/java/sample/weatherapp/controllers/ForecastDiagramController.java
@@ -1,13 +1,18 @@
 package sample.weatherapp.controllers;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Function;
 import java.util.prefs.Preferences;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -17,45 +22,36 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import sample.weatherapp.exceptions.HttpResponseException;
 import sample.weatherapp.models.Forecast;
+import sample.weatherapp.models.WeatherSummary;
 import sample.weatherapp.services.WeatherDataParser;
 
 public class ForecastDiagramController {
 
-  @FXML
-  private TabPane tabPane;
-  @FXML
-  private ScrollPane scrollPane;
-  @FXML
-  private LineChart<String, Number> tempChart;
-  @FXML
-  private CategoryAxis tempXAxis;
-  @FXML
-  private NumberAxis tempYAxis;
-  @FXML
-  private LineChart<String, Number> windSpeedChart;
-  @FXML
-  private CategoryAxis windSpeedXAxis;
-  @FXML
-  private NumberAxis windSpeedYAxis;
-  @FXML
-  private LineChart<String, Number> pressureChart;
-  @FXML
-  private CategoryAxis pressureXAxis;
-  @FXML
-  private NumberAxis pressureYAxis;
-  @FXML
-  private LineChart<String, Number> humidityChart;
-  @FXML
-  private CategoryAxis humidityXAxis;
-  @FXML
-  private NumberAxis humidityYAxis;
+  @FXML private TabPane tabPane;
+  @FXML private ScrollPane scrollPane;
+  @FXML private LineChart<String, Number> tempChart;
+  @FXML private CategoryAxis tempXAxis;
+  @FXML private NumberAxis tempYAxis;
+  @FXML private LineChart<String, Number> windSpeedChart;
+  @FXML private CategoryAxis windSpeedXAxis;
+  @FXML private NumberAxis windSpeedYAxis;
+  @FXML private LineChart<String, Number> pressureChart;
+  @FXML private CategoryAxis pressureXAxis;
+  @FXML private NumberAxis pressureYAxis;
+  @FXML private LineChart<String, Number> humidityChart;
+  @FXML private CategoryAxis humidityXAxis;
+  @FXML private NumberAxis humidityYAxis;
 
   private ResourceBundle rb;
   private String[] chartNames;
   private String[] yAxisLabels;
   private MainAppController parentController;
-  private String jsonResponse;
+
+  private final DateTimeFormatter dtfAPI = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+  private final DateTimeFormatter dtfChart = DateTimeFormatter.ofPattern("EEE d.\nHH:mm");
 
   @FXML
   private void initialize() {
@@ -72,19 +68,21 @@ public class ForecastDiagramController {
 
     rb = ResourceBundle.getBundle("localization");
 
-    chartNames = new String[] {
-        rb.getString("temperature"),
-        rb.getString("windSpeed"),
-        rb.getString("pressure"),
-        rb.getString("humidity")
-    };
+    chartNames =
+        new String[] {
+          rb.getString("temperature"),
+          rb.getString("windSpeed"),
+          rb.getString("pressure"),
+          rb.getString("humidity")
+        };
 
-    yAxisLabels = new String[] {
-        rb.getString("temperatureC"),
-        rb.getString("windSpeedMs"),
-        rb.getString("pressureHpa10"),
-        rb.getString("humidityPercent")
-    };
+    yAxisLabels =
+        new String[] {
+          rb.getString("temperatureC"),
+          rb.getString("windSpeedMs"),
+          rb.getString("pressureHpa10"),
+          rb.getString("humidityPercent")
+        };
 
     tempChart.setTitle(chartNames[0]);
     windSpeedChart.setTitle(chartNames[1]);
@@ -102,53 +100,77 @@ public class ForecastDiagramController {
     tabPane.getTabs().get(3).setText(chartNames[3]);
   }
 
-  @FXML
+  // Method to fetch weather data using virtual threads
   void updateForecast(TextField cityTextField) {
+
     String city = cityTextField.getText();
-    try {
-      jsonResponse = parentController.getWeatherApiClient().getWeatherForecastByCityName(city);
-      List<Forecast> forecasts = WeatherDataParser.parseForecastList(jsonResponse);
-      updateForecastChart(forecasts);
-    } catch (Exception e) {
-      e.printStackTrace();
+    ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+    Task<List<Forecast>> task =
+        new Task<>() {
+          @Override
+          protected List<Forecast> call() throws HttpResponseException, IOException {
+
+            // Show we are using Virtual Threads
+            System.out.println("Current thread: " + Thread.currentThread());
+
+            String jsonResponse =
+                parentController.getWeatherApiClient().getWeatherForecastByCityName(city);
+            List<Forecast> forecasts = WeatherDataParser.parseForecastList(jsonResponse);
+
+            return forecasts;
+          }
+        };
+
+    // Update the UI with the fetched data
+    task.setOnSucceeded(
+        event -> {
+          List<Forecast> forecasts = task.getValue();
+          Platform.runLater(
+              () -> {
+                var dataExtractors = initializeEmptyCharts();
+                plotForecastValues(forecasts, dataExtractors);
+              });
+        });
+
+    // Handle any exceptions that occurred during the task execution
+    task.setOnFailed(
+        event -> {
+          Throwable exception = task.getException();
+          exception.printStackTrace();
+          Platform.runLater(this::initializeEmptyCharts);
+        });
+
+    // Submit the task to the virtual thread executor
+    executor.submit(task);
+
+    // FixMe: get Executor from main
+    if (executor != null) {
+      executor.shutdown();
     }
   }
 
-  private void updateForecastChart(List<Forecast> forecasts) {
-    Function<Forecast, Number>[] dataExtractors = new Function[] {
-        forecast -> ((Forecast)forecast).temp(),
-        forecast -> ((Forecast)forecast).windSpeed(),
-        forecast -> ((Forecast)forecast).pressure() / 10.0,
-        forecast -> ((Forecast)forecast).humidity()
-    };
+  private Function<Forecast, Number>[] initializeEmptyCharts() {
+    Function<Forecast, Number>[] dataExtractors =
+        new Function[] {
+          forecast -> ((Forecast) forecast).temp(),
+          forecast -> ((Forecast) forecast).windSpeed(),
+          forecast -> ((Forecast) forecast).pressure() / 10.0,
+          forecast -> ((Forecast) forecast).humidity()
+        };
 
-    DateTimeFormatter dtfAPI = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    DateTimeFormatter dtfChart = DateTimeFormatter.ofPattern("EEE d.\nHH:mm");
-
-    TabPane tabPane = new TabPane();
+    tabPane.getTabs().clear();
 
     for (int i = 0; i < chartNames.length; i++) {
-      XYChart.Series<String, Number> series = new XYChart.Series<>();
-      series.setName(chartNames[i]);
-
-      for (Forecast forecast : forecasts) {
-        LocalDateTime dateTime = LocalDateTime.parse(forecast.dtTxt(), dtfAPI);
-        String xLabel = dateTime.format(dtfChart);
-        series.getData().add(new XYChart.Data<>(xLabel, dataExtractors[i].apply(forecast)));
-      }
-
       CategoryAxis xAxis = new CategoryAxis();
       xAxis.setLabel("");
-      xAxis.setCategories(FXCollections.observableArrayList(
-          series.getData().stream().map(XYChart.Data::getXValue).toList()
-      ));
       xAxis.setTickLabelRotation(45);
 
       NumberAxis yAxis = new NumberAxis();
       yAxis.setLabel(yAxisLabels[i]);
 
       LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
-      chart.getData().add(series);
+      chart.setTitle(chartNames[i]);
       chart.setLegendVisible(false);
       chart.setPrefWidth(2000);
 
@@ -161,6 +183,32 @@ public class ForecastDiagramController {
     scrollPane.setContent(tabPane);
     scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
     scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+    return dataExtractors;
+  }
+
+  private void plotForecastValues(
+      List<Forecast> forecasts, Function<Forecast, Number>[] dataExtractors) {
+
+    for (int i = 0; i < chartNames.length; i++) {
+      XYChart.Series<String, Number> series = new XYChart.Series<>();
+      series.setName(chartNames[i]);
+
+      for (Forecast forecast : forecasts) {
+        LocalDateTime dateTime = LocalDateTime.parse(forecast.dtTxt(), dtfAPI);
+        String xLabel = dateTime.format(dtfChart);
+        series.getData().add(new XYChart.Data<>(xLabel, dataExtractors[i].apply(forecast)));
+      }
+
+      LineChart<String, Number> chart =
+          (LineChart<String, Number>) tabPane.getTabs().get(i).getContent();
+      chart.getData().clear();
+      chart.getData().add(series);
+
+      // Set the tick label rotation again
+      CategoryAxis xAxis = (CategoryAxis) chart.getXAxis();
+      xAxis.setTickLabelRotation(45);
+    }
   }
 
   public void setParentController(MainAppController parentController) {
