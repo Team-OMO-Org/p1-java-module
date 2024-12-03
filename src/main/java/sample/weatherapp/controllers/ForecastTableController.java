9d3d86d0f4833a8f9e5e906cbd95d686699ce51d
olgaDCI <olga.babina@dci-student.org>
2024-12-03 16:42:22 +0100

    Use virtual threads for forecasttableview

--- src/main/java/sample/weatherapp/controllers/ForecastTableController.java
+++ src/main/java/sample/weatherapp/controllers/ForecastTableController.java
@@ -1,9 +1,14 @@
 package sample.weatherapp.controllers;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
@@ -12,9 +17,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.layout.HBox;
+import sample.weatherapp.exceptions.HttpResponseException;
+import sample.weatherapp.models.WeatherSummary;
 import sample.weatherapp.services.WeatherApiClient;
 import sample.weatherapp.models.DailyForecastWrapper;
 import sample.weatherapp.models.DailyForecastRoot;
@@ -107,27 +115,58 @@ public class ForecastTableController {
     forecastTableView.setPlaceholder(new Label(""));
   }
 
-  public void updateForecast(String city) {
-    Locale locale = Locale.getDefault(); // ??
+  protected void updateForecast(String city) {
+    Locale locale = Locale.getDefault();
+    ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
-    try {
-      String httpResponse =
-          parentController.getWeatherApiClient().getWeatherForecastByCityName(city);
-      if (httpResponse.isEmpty() || httpResponse.contains("error")) {
-        parentController.displayErrorAlert(httpResponse);
-        return;
-      }
-      DailyForecastRoot forecastData = WeatherDataParser.parseWeatherForecastData(httpResponse);
+    Task<List<DailyForecastWrapper>> task =
+        new Task<>() {
+          @Override
+          protected List<DailyForecastWrapper> call() throws Exception {
 
-      List<DailyForecastWrapper> wrappedForecasts =
-          forecastData.forecasts().stream()
-              .map(forecast -> new DailyForecastWrapper(forecast, locale))
-              .collect(Collectors.toList());
-      forecastTableView.getItems().setAll(wrappedForecasts);
+            String httpResponse =
+                parentController.getWeatherApiClient().getWeatherForecastByCityName(city);
+            DailyForecastRoot forecastData = WeatherDataParser.parseWeatherForecastData(httpResponse);
 
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+            List<DailyForecastWrapper> wrappedForecasts =
+                forecastData.forecasts().stream()
+                    .map(forecast -> new DailyForecastWrapper(forecast, locale))
+                    .collect(Collectors.toList());
+
+
+            // Show we are using Virtual Threads
+            System.out.println("Current thread: " + Thread.currentThread());
+
+            return wrappedForecasts;
+          }
+        };
+
+    // Update the UI with the fetched data
+    task.setOnSucceeded(
+        event -> {
+          List<DailyForecastWrapper> wrappedForecasts = task.getValue();
+          Platform.runLater(
+              () -> {
+                forecastTableView.getItems().setAll(wrappedForecasts);
+              });
+        });
+
+    // Handle any exceptions that occurred during the task execution
+    task.setOnFailed(
+        event -> {
+          Throwable exception = task.getException();
+          exception.printStackTrace();
+          Platform.runLater(this::initializeEmptyTable);
+        });
+
+    // Submit the task to the virtual thread executor
+    executor.submit(task);
+  }
+
+
+  public void initializeEmptyTable() {
+    forecastTableView.getItems().clear();
+    forecastTableView.setPlaceholder(new Label("Error fetching weather data"));
   }
 
   public void setParentController(MainAppController parentController) {
